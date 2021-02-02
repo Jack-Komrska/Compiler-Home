c56a3bfd96b8e870483cff614c086388b3dc4209
Jack-Komrska <jktennesseejk123@gmail.com>
2021-02-02 13:18:05 -0500

    first token implementation
    Still needs a ton of work, but have the foundation for the scanner. Need to flesh more of it out and fancy up the logic

--- CompilerHome/Compiler/Main.cpp
+++ CompilerHome/Compiler/Main.cpp
@@ -5,30 +5,56 @@
 
 using namespace std;
 
-int main()
+
+// not printing each of the identifiers individually, (needs to stop printing out the entire file), need to make a reserved table of some sorts (ask Nick, read some more), add more cases to switch
+// and need to add proper logic for each of the cases
+
+
+
+static Token ScanToken(ifstream& file, int& lineNum)
 {
 	char currCh;
 	char nextCh;
+	Token* token = new Token();
+
+	while (!file.eof())
+	{
+		currCh = file.get(); //get each character individually
+
+		switch (currCh) {
+		case 'a': case 'b': case 'c': case 'd': case 'e': case 'f': case 'g': case 'h': case 'i': case 'j': case 'k': case 'l': case 'm': case 'n': case 'o': case 'p': case 'q': case 'r': case 's': case 't': case 'u': case 'v': case 'w': case 'x': case 'y': case 'z':
+			token->type = id;
+			token->val.stringVal[0] = currCh;
+		}
+
+		//file.unget(); //to make the reader go back the previous character
+
+		if (currCh == '\n')
+		{
+			//cout << " - " << linenum; //prints the current line number
+			lineNum += 1; //updates the current line number
+		}
+
+		cout << currCh;
+	}
+
+	return *token;
+}
+
+int main()
+{
 	int lineNum = 1;
 	ifstream file;
+	
 	file.open("test.txt");
 	if (file.is_open())
 	{
-		while (!file.eof())
-		{
-			currCh = file.get(); //get each character individually
-			
-			//file.unget(); //to make the reader go back the previous character
-
-			if (currCh == '\n')
-			{
-				//cout << " - " << lineNum; //prints the current line number
-				lineNum += 1; //updates the current line number
-			}
-
-			cout << currCh;
-		}
+		Token temp;
+		temp = ScanToken(file, lineNum);
+		cout << temp.type;
+		cout << temp.val.stringVal;
 	}
 	
 	file.close();
 }
+
--- CompilerHome/Compiler/Token.h
+++ CompilerHome/Compiler/Token.h
@@ -1,11 +1,12 @@
 #include <string>
 
 using namespace std;
+//operators, keyword, identifier, numeral, character, str
+enum reservedWord {id, num_integer, num_double, character, str, assign_op, mult_op, add_op, sub_op, exp_op, div_op, rem_op, key_for, key_if, key_return, key_else, key_and, key_or, key_const, sym_sc, sym_lparen, sym_rparen, eof, unknown};
 
-enum partOfSpeech {operators, keyword, identifier, numeral, character, str};
-
-class TokenType { //token type class, holds what token it is and the value
-	partOfSpeech token;
+class Token { //token type class, holds what token it is and the value
+public:
+	reservedWord type;
 	union { //holds the value depending on the type
 		char stringVal[256];
 		int intVal;
