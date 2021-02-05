ac6bfd12a53623a18b86699f18ba7b52aafe7da5
Jack-Komrska <jktennesseejk123@gmail.com>
2021-02-05 12:22:12 -0500

    Scanner Done
    Scanner completion (not fully tested, but prototype seems to work great)

--- CompilerHome/Compiler/Token.h
+++ CompilerHome/Compiler/Token.h
@@ -2,7 +2,10 @@
 
 using namespace std;
 //operators, keyword, identifier, numeral, character, str
-enum reservedWord {id, num_integer, num_double, character, str, assign_op, mult_op, add_op, sub_op, exp_op, div_op, rem_op, key_for, key_if, key_return, key_else, key_and, key_or, key_const, sym_sc, sym_lparen, sym_rparen, eof, unknown};
+enum reservedWord
+{
+	id, num_integer, num_double, character, str, assign_op, equal_op, mult_op, add_op, sub_op, exp_op, div_op, rem_op, dot_op, key_for, key_if, key_return, key_else, key_and, key_or, key_const, sym_sc, sym_lparen, sym_rparen, sym_less, sym_great, eof, unknown
+};
 
 class Token { //token type class, holds what token it is and the value
 public:
